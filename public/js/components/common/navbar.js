414163a646cc0251af499af89a5892743bea9696
cakou <carole.hermouet@gmail.com>
2017-07-18 16:22:22 +0200

    modification minime horloge

--- public/js/components/common/navbar.js
+++ public/js/components/common/navbar.js
@@ -1,6 +1,8 @@
+import moment from 'moment'
+
 let navbar = {
     templateUrl: 'js/components/common/navbar.html',
-    controller: ['UsersService', '$state', function(UsersService, $state) {
+    controller: ['UsersService', '$state', '$interval', function(UsersService, $state, $interval) {
         'use strict'
         angular.extend(this, {
             $onInit() {
@@ -19,6 +21,13 @@ let navbar = {
             }
 
         })
+        let dateChange = () => {
+            this.date = moment().format('MMMM Do YYYY, h:mm:ss a')
+
+        }
+        dateChange();
+        $interval(dateChange, 1000)
+
     }]
 }
 
